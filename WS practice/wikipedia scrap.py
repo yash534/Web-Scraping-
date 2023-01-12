13b6dbaa70ba34806fde99b1bcba8cd79dfccd19
Yash pandey <66487068+yash534@users.noreply.github.com>
2023-01-13 00:35:18 +0530

    Add files via upload
    Data that are scraped in this file that are belongs to the wikipedia of Aliexpress.com

---
+++ WS practice/wikipedia scrap.py
@@ -0,0 +1,32 @@
+#importing beautifulSoup library for scrapping.
+from bs4 import BeautifulSoup
+#importing requests library for sending request to url for getting imformation from their website.
+import requests
+
+#sending request to website.
+html_text = requests.get('https://en.wikipedia.org/wiki/AliExpress').text
+#using lxml parser for parsing.
+soup = BeautifulSoup(html_text, 'lxml')
+
+#scrapping data using class of that element that are available on this url.
+heading = soup.find_all('span', class_ = 'mw-page-title-main')
+sidebar_links = soup.find('li', class_ = 'mw-list-item').text.replace(' ','')
+headline = soup.find_all('span', class_ = 'mw-headline')
+
+#scraping all the href elements present in the given url.
+urls = []
+for link in soup.find_all('a'):
+    print(link.get('href'))
+    
+print(f'''
+Heading:: {heading}
+Sidebar Links:: {sidebar_links}
+Headline:: {headline}
+''')
+
+
+
+
+
+
+
